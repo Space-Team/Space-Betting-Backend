2483eb82bca0c7aea359aada4928af43712f607f
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2018-02-05 15:51:29 -0700

    Write list queries

--- app.js
+++ app.js
@@ -7,9 +7,41 @@ const queries = require('./queries');
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.sendStatus(200);
+app.get('/user-bets/:id', (req, res) => {
+  queries
+    .creatorBets(req.params.id)
+    .then(bets => {
+      res.json({bets});
+    })
+    .catch(console.error);
 });
+
+app.get('/users', (req, res) => {
+  queries
+    .list('users')
+    .then(users => {
+      res.json({users});
+    })
+    .catch(console.error);
+});
+
+app.get('/bets', (req, res) => {
+  queries
+    .list('bets')
+    .then(bets => {
+      res.json({bets});
+    })
+    .catch(console.error);
+});
+
+// app.post('/', (req, res) => {
+//   queries
+//     .create(req.body)
+//     .then(res => {
+//       res.status(201).json({resolution: resolution});
+//     })
+//     .catch(console.error);
+// });
 
 app.use((req, res) => {
   res.sendStatus(404);
---
+++ queries.js
@@ -0,0 +1,37 @@
+const database = require('./database-connection');
+
+module.exports = {
+  list(table) {
+    return database(table);
+  },
+  creatorBets(id) {
+    return database('bets')
+      .join('users', 'users.id', '=', 'bets.creator')
+      .select('users.name', 'bets.id', 'bets.description', 'bets.amount', 'bets.accepted', 'bets.resolved', 'bets.winner', 'bets.date', 'bets.comment')
+      .where('users.id', id)
+      .returning('*');
+  }
+  // read(id) {
+  //   return database('resolution')
+  //     .where('id', id)
+  //     .first();
+  // },
+  // create(resolution) {
+  //   return database('resolution')
+  //     .insert(resolution)
+  //     .returning('*')
+  //     .then(record => record[0]);
+  // },
+  // update(id, resolution) {
+  //   return database('resolution')
+  //     .update(resolution)
+  //     .where('id', id)
+  //     .returning('*')
+  //     .then(record => record[0]);
+  // },
+  // delete(id) {
+  //   return database('resolution')
+  //     .where('id', id)
+  //     .del();
+  // }
+};
